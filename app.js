d85e699c5420aec34d9d46e423cb3e95490bbed0
TamarKhutchua <126088392+TamarKhutchua@users.noreply.github.com>
2023-09-01 14:01:53 +0400

    break out

--- app.js
+++ app.js
@@ -60,8 +60,25 @@ console.log("It is " + day);
 //   i--;
 // }
 
-let i = 2;
-do {
-  i *= 2;
-  console.log("Multiplied Number: " + i);
-} while (i < 1000);
+// let i = 2;
+// do {
+//   i *= 2;
+//   console.log("Multiplied Number: " + i);
+// } while (i < 1000);
+
+let i = 0;
+let j = 0;
+for (i = 1; i < 3; i++) {
+  console.log("Outer Loop: " + i);
+  for (j = 1; j < 4; j++) {
+    if (i === 1 && j === 2) {
+      console.log("\tSkipped: " + j);
+      continue;
+    }
+    if (i === 2 && j === 2) {
+      console.log("\tBroken: " + j);
+      break;
+    }
+    console.log("\tInner Loop: " + j);
+  }
+}
